875501f98ece67f2bc9f30d7b700375567a03bc5
pink <jerry.zheng@cloudkeeper.cn>
2020-08-13 18:03:10 +0800

    '完成一个button组件'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
-function App() {
+const App: React.FC = () => {
     return (
         <div className='App'>
             <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
+                <Button
+                    className='sd'
+                    autoFocus
+                    onClick={(e) => {
+                        e.preventDefault()
+                        alert(1)
+                    }}
+                >
+                    Hello
+                </Button>
+                <Button size={ButtonSize.Large}>Hello</Button>
+                <Button size={ButtonSize.Small}>Hello</Button>
+                <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+                    Hello
+                </Button>
+                <Button disabled btnType={ButtonType.Link} href='http://www.baidu.com'>
+                    Baidu Link
+                </Button>
+                <Button btnType={ButtonType.Link} href='http://www.baidu.com' target='_blank'>
+                    Baidu Link
+                </Button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
