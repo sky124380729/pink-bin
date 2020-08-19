88c99c0098345709fe93b3536ba35e4a2c361106
pink <jerry.zheng@cloudkeeper.cn>
2020-08-19 14:55:03 +0800

    menu优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Alert from './components/Alert/alert'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 const App: React.FC = () => {
     return (
         <div className='App'>
             <header className='App-header'>
+                <Menu
+                    defaultIndex={0}
+                    mode='vertical'
+                    onSelect={(index) => {
+                        alert(index)
+                    }}
+                >
+                    <MenuItem index={0}>cool link</MenuItem>
+                    <MenuItem index={1} disabled>
+                        cool link2
+                    </MenuItem>
+                    <MenuItem index={2}>cool link3</MenuItem>
+                </Menu>
+
                 <div>
                     <Button
                         className='sd'
